38ebe1bb512fae27afea6771980956c542793461
JoaquimLP <joaquimlp@gmail.com>
2020-04-27 21:46:32 -0300

    Correção do exercicio 13

--- Lista04/ex013.py
+++ Lista04/ex013.py
@@ -11,20 +11,6 @@ crime. As perguntas são:
     ser classificada como "Suspeita", entre 3 e 4 como "Cúmplice" e 5 como
     "Assassino". Caso contrário, ele será classificado como "Inocente".
 """
-"""
-21. Faça um programa que faça 5 perguntas para uma pessoa sobre um crime. As perguntas
-são:
-        • "Telefonou para a vítima?"
-        • "Esteve no local do crime?"
-        • "Mora perto da vítima?"
-        • "Devia para a vítima?"
-        • "Já trabalhou com a vítima?"
-    O programa deve no final emitir uma classificação sobre a participação da pessoa
-    no crime.
-        • Se a pessoa responder positivamente a 2 questões ela deve ser classificada como
-        "Suspeita", entre 3 e 4 como "Cúmplice" e 5 como "Assassino". Caso contrário, ele
-        será classificado como "Inocente".
-"""
 from time import  sleep
 print("Vou lhe Fazer 5 perguntas sobre o crime ocorrido digite [S] sim é [N] para não")
 p1 = input("Telefonou para a vítima? (S/N) ").upper()
